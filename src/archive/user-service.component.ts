000c673b3574056a7200b1bbdfc5e9612c5b1112
MadAngular <roro555000ro@gmail.com>
2025-04-27 09:30:20 +0300

    Update user-service.component.ts

--- src/archive/user-service.component.ts
+++ src/archive/user-service.component.ts
@@ -1,38 +1,38 @@
-// import { Injectable } from '@angular/core';
-// import { BehaviorSubject } from 'rxjs';
-// import { User } from '../app/Interfaces/user.interface';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { User } from '../app/Interfaces/user.interface';
 
-// @Injectable({ providedIn: 'root' })
-// export class UsersService {
+@Injectable({ providedIn: 'root' })
+export class UsersService {
   
-//   usersSubject = new BehaviorSubject<User[]>([]);
+  usersSubject = new BehaviorSubject<User[]>([]);
   
-//   setUser(users: User[]) {
-//     this.usersSubject.next(users);
-//   }
+  setUser(users: User[]) {
+    this.usersSubject.next(users);
+  }
   
-//   editedUser(editUser: User) {
-//     this.usersSubject.next(
-//       this.usersSubject.value.map((user) =>
-//         user.id === editUser.id ? { ...editUser } : user
-//       )
-//     );
-//   }
+  editedUser(editUser: User) {
+    this.usersSubject.next(
+      this.usersSubject.value.map((user) =>
+        user.id === editUser.id ? { ...editUser } : user
+      )
+    );
+  }
 
-//   deleteUser(id: number) {
-//     this.usersSubject.next(
-//       this.usersSubject.value.filter((item) => {
-//         if (id === item.id) {
-//           return false;
-//         } else {
-//           return true;
-//         }
-//       })
-//     );
-//   }
+  deleteUser(id: number) {
+    this.usersSubject.next(
+      this.usersSubject.value.filter((item) => {
+        if (id === item.id) {
+          return false;
+        } else {
+          return true;
+        }
+      })
+    );
+  }
 
-//   createUser(user: User) {
-//     this.usersSubject.next([...this.usersSubject.value, user]);
-//   }
+  createUser(user: User) {
+    this.usersSubject.next([...this.usersSubject.value, user]);
+  }
   
-// }
+}
