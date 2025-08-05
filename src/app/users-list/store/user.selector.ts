0461c5866b56e2857d656fa8e83744a56aaa738a
MadAngular <roro555000ro@gmail.com>
2025-08-05 20:51:06 +0300

    feat new elements

---
+++ src/app/users-list/store/user.selector.ts
@@ -0,0 +1,21 @@
+import { createSelector } from "@ngrx/store";
+import { User } from "../../interface/users-interface";
+
+
+interface UserState{
+    users: User[];
+}
+
+
+interface AppState {
+    users: UserState;
+}
+
+
+export const selectUsersFeature = (state: AppState) => state.users;
+
+
+export const selectUsers = createSelector(
+    selectUsersFeature,
+    (state: UserState) => state.users
+);
--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -5,6 +5,9 @@ import { UsersApiService } from '../users-api.service';
 import { UserCardComponent } from './user-card/user-card.component';
 import { UsersService } from '../services/users.service';
 import { UserFormComponent } from './create-user-form/create-user-form.component';
+import { Store } from '@ngrx/store';
+import { UserActions } from './store/user.actions';
+import { selectUsers } from './store/user.selector';
 
 @Component({
   selector: 'app-users-list',
@@ -17,6 +20,8 @@ import { UserFormComponent } from './create-user-form/create-user-form.component
 export class UsersListComponent {
   readonly usersApiService = inject(UsersApiService);
   readonly usersService = inject(UsersService);
+  private readonly store = inject(Store);
+  public readonly users$ = this.store.select(selectUsers)
 
   users = this.usersService.users;
 
@@ -25,29 +30,31 @@ export class UsersListComponent {
       this.usersService.setUsers(response);
 
       this.usersService.usersSubject.subscribe((users) => (this.users = users));
+      this.store.dispatch(UserActions.set({ users: response }));
     });
   }
   onDelete(id: number) {
     this.usersService.deleteUser(id);
+    this.store.dispatch(UserActions.delete({ id }));
   }
 
   createUser(user: any) {
-    this.usersService.createUser({
-      id: new Date().getTime(),
-      name: user.name,
-      email: user.email,
-      website: user.website,
-      company: {
-        name: user.companyName,
-      },
-    });
+    this.store.dispatch(
+      UserActions.create({
+        user: {
+          id: new Date().getTime(),
+          name: user.name,
+          email: user.email,
+          website: user.website,
+          company: {
+            name: user.companyName,
+          },
+        },
+      })
+    );
   }
+
   editUser(user: any) {
-    this.usersService.editUser({
-      ...user,
-      company: {
-        name: user.companyName,
-      },
-    });
+    this.store.dispatch(UserActions.edit({ user }));
   }
 }
